908a0fb69d12bc04d55ef22abbf05fcbc81f48cf
Stas_T <stavakalov@inbox.ru>
2018-11-04 20:38:29 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static int field;
     public static String bluee;
-    static String a;
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
 }
